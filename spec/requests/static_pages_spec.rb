bb4822bbc8ffed2a1dde7d859451abf17cac9153
fschwiet <fschwiet@gmail.com>
2012-10-02 21:01:55 -0700

    add new page / test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,7 @@ describe "StaticPages" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get '/static_pages/home'
       response.status.should be(200)
+      visit '/static_pages/home'
       page.should have_content("Sample App");
     end
   end
